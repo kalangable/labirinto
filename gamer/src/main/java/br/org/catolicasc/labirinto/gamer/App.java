349b6e2415c1376eec0ff15e4bda9d164475a4de
adilson.krasovski <adilson.krasovski@joic0428.catolicasc.org.br>
2014-05-07 20:57:28 -0300

    Incluindo log4j no projeto

--- gamer/src/main/java/br/org/catolicasc/labirinto/gamer/App.java
+++ gamer/src/main/java/br/org/catolicasc/labirinto/gamer/App.java
@@ -1,5 +1,8 @@
 package br.org.catolicasc.labirinto.gamer;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
 import br.org.catolicasc.labirinto.core.Controle;
 import br.org.catolicasc.labirinto.view.Labirinto;
 import br.org.catolicasc.labirinto.view.factory.FactoryLabirinto;
@@ -9,7 +12,12 @@ import br.org.catolicasc.labirinto.view.factory.FactoryLabirinto;
  * 
  */
 public class App {
+//	static {
+//		PropertyConfigurator.configure(Thread.currentThread().getContextClassLoader().getResource("log4j.properties"));
+//	}
+	private static final Logger LOG = Logger.getLogger(App.class);
 	public static void main(String[] args) {
+		LOG.debug("Start da Aplicação");
 		Labirinto labirinto = FactoryLabirinto.createLabirinto(Thread.currentThread().getContextClassLoader().getResourceAsStream("lab001.txt"));
 		new Controle(new Rato(labirinto.getBegin()), labirinto);
 	}
