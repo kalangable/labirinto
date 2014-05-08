68e968072bbb38f98c7a4cef38c37b07e93861ba
adilson.krasovski <adilson.krasovski@joic0428.catolicasc.org.br>
2014-05-07 22:04:40 -0300

    Inclusão de tratamento de erro para quando o arquivo não exixtir ou para qualquer erro no processamento do arquivo

--- gamer/src/main/java/br/org/catolicasc/labirinto/gamer/App.java
+++ gamer/src/main/java/br/org/catolicasc/labirinto/gamer/App.java
@@ -1,5 +1,9 @@
 package br.org.catolicasc.labirinto.gamer;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
@@ -19,7 +23,49 @@ public class App {
 
 	public static void main(String[] args) {
 		LOG.debug("Start da Aplicação");
-		Labirinto labirinto = FactoryLabirinto.createLabirinto(Thread.currentThread().getContextClassLoader().getResourceAsStream("lab001.txt"));
+		Labirinto labirinto = null;
+		File arquivoEntrada = null;
+
+		/**
+		 * Quando não localizar o arquivo no resources, solicito ao usuário que
+		 * fornceça o arquivo
+		 */
+		try {
+			arquivoEntrada = new File(Thread.currentThread().getContextClassLoader().getResource("lab001.txt").getFile());
+		} catch (NullPointerException e) {
+			arquivoEntrada = getFileInformationOfUser();
+		}
+		/**
+		 * Se houver algum erro no processamento do arquivo vou encerrar a
+		 * aplicação
+		 */
+		try {
+			labirinto = FactoryLabirinto.createLabirinto(arquivoEntrada);
+		} catch (IOException e) {
+			LOG.warn("Não foi possível processar o arquivo");
+			System.err.println("Erro no processamento do arquivo, verifique permissões no arquivo :" + arquivoEntrada.getAbsolutePath());
+			System.exit(127);
+		}
+
+		// Vou iniciar o Game
+
 		new Controle(new Rato(labirinto.getBegin()), labirinto);
+	}
+
+	/**
+	 * Mantém o usuário em loop até que seja informado um arquivo válido
+	 * 
+	 * @return Arquivo informado pelo usuário
+	 */
+	private static File getFileInformationOfUser() {
+		File arquivoEntrada = null;
+		while (arquivoEntrada == null || !arquivoEntrada.exists()) {
+			LOG.fatal("Não achei o arquivo");
+			System.err.println("Arquivo não localizado, por favor forneçao o caminho do arquivo");
+			Scanner leitura = new Scanner(System.in);
+			String caminhoArquivo = leitura.nextLine();
+			arquivoEntrada = new File(caminhoArquivo);
+		}
+		return arquivoEntrada;
 	}
 }
